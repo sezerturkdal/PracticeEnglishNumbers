b7fda452431e188d830d0c57dacae329b0b7300e
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-11-15 20:09:41 +0300

    Initial Commit

---
+++ EnglishNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnglishNumbers
+//
+//  Created by Sezer on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
